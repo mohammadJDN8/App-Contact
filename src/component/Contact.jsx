e7746cc980a647dff26d4193340da0bf5c2ee937
mohammad jamshidia <mohammad_jamshida@yahoo.com>
2023-11-14 00:54:41 +0330

    update contact base

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,14 +1,53 @@
-
+import { useState } from "react";
 function Contact() {
+  const [contact, setContact] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setContact((contact) => ({ ...contact, [name]: value }));
+    console.log(name, value);
+  };
+  const addHandler = () => {
+    console.log(contact)
+  }
   return (
     <div>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Phone" />
-      <button>Add contact</button>
+      <input
+        type="text"
+        placeholder="Name"
+        value={contact.name}
+        onChange={changeHandler}
+        name="name"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={contact.lastName}
+        onChange={changeHandler}
+        name="lastName"
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        value={contact.email}
+        onChange={changeHandler}
+        name="email"
+      />
+      <input
+        type="number"
+        placeholder="Phone"
+        value={contact.phone}
+        onChange={changeHandler}
+        name="phone"
+      />
+      <button onClick={addHandler}>Add contact</button>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
